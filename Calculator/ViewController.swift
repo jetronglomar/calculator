4ca952ca580321b9812a09e8667e1c17270f2757
Jet Ron Glomar <jetronglomar@gmail.com>
2019-12-04 09:25:14 +0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by ron on 12/4/19.
+//  Copyright © 2019 Jet Ronrick Glomar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
